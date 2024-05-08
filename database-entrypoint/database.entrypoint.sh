1f9554cf4db188cb62611bb6fd3f986d4b92910d
jimmg35 <jim60308@gmail.com>
2024-05-08 12:36:20 +0800

    first commit

---
+++ database-entrypoint/database.entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+# 啟動 PostgreSQL 並創建資料庫
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE DATABASE $APP_DB_NAME;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $POSTGRES_USER;
+EOSQL
+
+# 連接到新建的資料庫
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$APP_DB_NAME" <<-EOSQL
+  BEGIN;
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    CREATE EXTENSION postgis;
+  COMMIT;
+EOSQL
